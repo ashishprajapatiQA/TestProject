1b36467e72f8bc633b74875fb787e7681b1298ff
ashishprajapatiQA <102219975+ashishprajapatiQA@users.noreply.github.com>
2025-10-23 11:15:24 +0530

    Remove noscript Google Tag Manager code
    Removed Google Tag Manager (noscript) code from script.js.

--- script.js
+++ script.js
@@ -25,10 +25,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-NZWF3WV9');
 <!-- End Google Tag Manager -->
 
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZWF3WV9"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+
 
   
 // intercom
