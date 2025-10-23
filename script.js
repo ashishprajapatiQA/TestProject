7d0b6cd4a7049d8c2f4e7b9ac5e021a48e4b3ec9
ashishprajapatiQA <102219975+ashishprajapatiQA@users.noreply.github.com>
2025-10-23 11:08:00 +0530

    Fix Google Tag Manager script syntax

--- script.js
+++ script.js
@@ -18,11 +18,11 @@
 
 // GTM - QA 
 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NZWF3WV9');</script>
+})(window,document,'script','dataLayer','GTM-NZWF3WV9');
 <!-- End Google Tag Manager -->
 
 // intercom
